40c25378cd89da495ae5cd8776233b89869d7321
metjyc <109847500+metjyc@users.noreply.github.com>
2025-10-18 21:44:40 +0900

    [Bronze V] Title: 구구단, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = input[0];
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${n} * ${i} = ${n * i}`);
+}
