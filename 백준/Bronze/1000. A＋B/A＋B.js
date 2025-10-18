20f9c14d983f68412970f41a40e0ae83d20f46e4
metjyc <109847500+metjyc@users.noreply.github.com>
2025-10-18 21:10:33 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const A = input[0];
+const B = input[1];
+
+console.log(A + B);
