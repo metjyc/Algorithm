c2dba5b784896e860e7a10397f02079597d5c005
metjyc <109847500+metjyc@users.noreply.github.com>
2025-10-18 21:41:15 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log("Hello World!");
