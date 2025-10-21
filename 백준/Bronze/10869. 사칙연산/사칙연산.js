9700072f73492cf25b6ab0a8740e180c3ec66957
metjyc <109847500+metjyc@users.noreply.github.com>
2025-10-21 14:48:30 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+const A = input[0];
+const B = input[1];
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(Math.floor(A / B));
+console.log(A % B);
